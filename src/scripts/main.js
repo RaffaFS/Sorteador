a772966a96e84d01045a35f3d771ceb0b720964e
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2023-12-06 08:57:45 -0300

    adicionando DOMContentLoaded no início do main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,40 @@
-document.getElementById('form-sorteador').addEventListener('submit', function(evento){
-    evento.preventDefault();
 
-    const ponto1 = "."
-    const ponto2 = ".."
-    const ponto3 = "..."
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
+        evento.preventDefault();
 
-    let numeroMaximo = document.getElementById('numero-maximo').value;
-    numeroMaximo = parseInt(numeroMaximo);
-    
-    let numeroAleatorio = Math.random() * numeroMaximo;
-    numeroAleatorio = Math.floor(numeroAleatorio + 1);
+        const ponto1 = "."
+        const ponto2 = ".."
+        const ponto3 = "..."
 
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = ponto1;
-    }, 100)
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = ponto2;
-    }, 600)
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = ponto3;
-    }, 1100)
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+        
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
 
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = ponto1;
-    }, 1600)
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = ponto2;
-    }, 2100)
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = ponto3;
-    }, 2600)
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = ponto1;
+        }, 100)
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = ponto2;
+        }, 600)
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = ponto3;
+        }, 1100)
 
-    setTimeout(function(){
-        document.getElementById('resultado-valor').innerText = numeroAleatorio;
-    }, 3600)
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = ponto1;
+        }, 1600)
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = ponto2;
+        }, 2100)
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = ponto3;
+        }, 2600)
+
+        setTimeout(function(){
+            document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        }, 3600)
+    })
 })
-
